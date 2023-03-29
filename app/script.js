3f1c73ed0592fe781a0291d09fc9f7b8402c9671
Eddie <eddie.amaya@analog.com>
2023-03-29 10:39:49 -0500

    new button

--- app/script.js
+++ app/script.js
@@ -1,30 +1,32 @@
 const deviceNameInput = document.getElementById('deviceNameInput');
-const controlButton = document.getElementById('controlButton');
+const connectButton = document.getElementById('connectButton');
 const connectionStatus = document.getElementById('connectionStatus');
-
-controlButton.addEventListener('click',BLEManager);
+const connectedDevice ;
+const device ;
+connectButton.addEventListener('click',BLEManager);
+sendButton.addEventListener('click',sendBLEData);
 
 async function BLEManager(){
     connectionStatus.textContent = "SEARCHING";
     try{
-       const device = await navigator.bluetooth.requestDevice({
+        device = await navigator.bluetooth.requestDevice({
         // acceptAllDevices:true
         filters: [{name: deviceNameInput.value,}],
         optionalServices: ["e0262760-08c2-11e1-9073-0e8ac72e1001"]
         }); 
 
-        const connectedDevice = await device.gatt.connect();
+        connectedDevice = await device.gatt.connect();
         connectionStatus.textContent = "CONNECTED";
-        const armPropDataService = await connectedDevice.getPrimaryService("e0262760-08c2-11e1-9073-0e8ac72e1001");
-        console.log("Services obtained");
-        const armPropDataCharacteristic = await armPropDataService.getCharacteristic("e0262760-08c2-11e1-9073-0e8ac72e0001");
-        console.log(armPropDataCharacteristic);
-        console.log("Characteristics discovered");
-        var uint8array = new TextEncoder().encode("edwin amaya");
-        setTimeout(function () {
-              armPropDataCharacteristic.writeValueWithoutResponse(uint8array);
-              console.log("Value has been written");
-        },5000);
+        // const armPropDataService = await connectedDevice.getPrimaryService("e0262760-08c2-11e1-9073-0e8ac72e1001");
+        // console.log("Services obtained");
+        // const armPropDataCharacteristic = await armPropDataService.getCharacteristic("e0262760-08c2-11e1-9073-0e8ac72e0001");
+        // console.log(armPropDataCharacteristic);
+        // console.log("Characteristics discovered");
+        // var uint8array = new TextEncoder().encode("edwin amaya");
+        // setTimeout(function () {
+        //       armPropDataCharacteristic.writeValueWithoutResponse(uint8array);
+        //       console.log("Value has been written");
+        // },5000);
       
     }
     catch {
@@ -36,6 +38,21 @@ async function BLEManager(){
         }
         
     }
-    
 
+}
+
+async function sendBLEData(){
+
+    const armPropDataService = await connectedDevice.getPrimaryService("e0262760-08c2-11e1-9073-0e8ac72e1001");
+    console.log("Services obtained");
+    const armPropDataCharacteristic = await armPropDataService.getCharacteristic("e0262760-08c2-11e1-9073-0e8ac72e0001");
+    console.log(armPropDataCharacteristic);
+    console.log("Characteristics discovered");
+    var uint8array = new TextEncoder().encode("edwin amaya");
+    
+    armPropDataCharacteristic.writeValueWithoutResponse(uint8array);
+    console.log("Value has been written");
+
+    
+    
 }
