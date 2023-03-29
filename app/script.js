58188d2a1c018e40067e47a39ea755e88d7fd7b8
Eddie <eddie.amaya@analog.com>
2023-03-29 15:27:46 -0500

    tweaks

--- app/script.js
+++ app/script.js
@@ -4,6 +4,7 @@ const stringToSend = document.getElementById('stringToSend');
 const connectButton = document.getElementById('connectButton');
 const sendButton = document.getElementById('sendButton');
 const connectionStatus = document.getElementById('connectionStatus');
+const logArea = document.getElementById('logArea');
 
 //  GLOBAL objects
 var connectedDevice = null;
@@ -16,96 +17,102 @@ connectButton.addEventListener('click', BLEManager);
 sendButton.addEventListener('click', sendBLEData);
 
 async function BLEManager() {
-  connectionStatus.textContent = 'SEARCHING';
-  try {
-    device = await navigator.bluetooth.requestDevice({
-      acceptAllDevices: true,
-      //   filters: [{
-      //     name: deviceNameInput.value,
-      //   }],
-      //  optionalServices: ['e0262760-08c2-11e1-9073-0e8ac72e1001']
-    });
-
-    connectedDevice = await device.gatt.connect();
-    connectionStatus.textContent = 'CONNECTED';
-
-    // setTimeout(function () {
-    //       armPropDataCharacteristic.writeValueWithoutResponse(uint8array);
-    //       console.log("Value has been written");
-    // },5000);
-
+    connectionStatus.textContent = 'SEARCHING';
     try {
-      console.log('Getting Services...');
-      const services = await connectedDevice.getPrimaryServices();
-
-      console.log('Getting Characteristics...');
-      for (const service of services) {
-        console.log('> Service: ' + service.uuid);
-        const characteristics = await service.getCharacteristics();
-
-        characteristics.forEach(characteristic => {
-          console.log(
-              '>> Characteristic: ' + characteristic.uuid + ' ' +
-              getSupportedProperties(characteristic));
+        device = await navigator.bluetooth.requestDevice({
+            acceptAllDevices: true,
+            //   filters: [{
+            //     name: deviceNameInput.value,
+            //   }],
+            //  optionalServices: ['e0262760-08c2-11e1-9073-0e8ac72e1001']
         });
-      }
-    } catch (error) {
-      console.log('Argh! ' + error);
-    }
 
-    // Discover ArmPropDataCharacteristic
-    armPropDataService = await connectedDevice.getPrimaryService(
-        'e0262760-08c2-11e1-9073-0e8ac72e1001');
-    console.log('Services obtained');
-    armPropDataCharacteristic = await armPropDataService.getCharacteristic(
-        'e0262760-08c2-11e1-9073-0e8ac72e0001');
-    console.log(armPropDataCharacteristic);
-    console.log('Characteristics discovered');
-    // Enable notifications on ARMPropDataCharacteristic
-    if (armPropDataCharacteristic.properties.notify) {
-        console.log("Notifications supported");
-        armPropDataCharacteristic.addEventListener("characteristicvaluechanged",handleNotifications);
+        connectedDevice = await device.gatt.connect();
+        connectionStatus.textContent = 'CONNECTED';
+
+        // setTimeout(function () {
+        //       armPropDataCharacteristic.writeValueWithoutResponse(uint8array);
+        //       logger("Value has been written");
+        // },5000);
+
+        try {
+            logger('Getting Services...');
+            const services = await connectedDevice.getPrimaryServices();
+
+            logger('Getting Characteristics...');
+            for (const service of services) {
+                logger('> Service: ' + service.uuid);
+                const characteristics = await service.getCharacteristics();
+
+                characteristics.forEach(characteristic => {
+                    logger(
+                        '>> Characteristic: ' + characteristic.uuid + ' ' +
+                        getSupportedProperties(characteristic));
+                });
+            }
+        } catch (error) {
+            logger('Argh! ' + error);
+        }
+
+        // Discover ArmPropDataCharacteristic
+        armPropDataService = await connectedDevice.getPrimaryService(
+            'e0262760-08c2-11e1-9073-0e8ac72e1001');
+        logger('Services obtained');
+        armPropDataCharacteristic = await armPropDataService.getCharacteristic(
+            'e0262760-08c2-11e1-9073-0e8ac72e0001');
+        logger(armPropDataCharacteristic);
+        logger('Characteristics discovered');
+        // Enable notifications on ARMPropDataCharacteristic
+        if (armPropDataCharacteristic.properties.notify) {
+            logger('Notifications supported');
+            armPropDataCharacteristic.addEventListener(
+                'characteristicvaluechanged', handleNotifications_arm_prop_data);
         }
         await armPropDataCharacteristic.startNotifications();
-    
 
-  } catch {
-    if (typeof device !== 'undefined') {
-      connectionStatus.textContent = 'CONNECTION FAILED';
-    } else {
-      connectionStatus.textContent = 'CANCELLED';
+
+    } catch {
+        if (typeof device !== 'undefined') {
+            connectionStatus.textContent = 'CONNECTION FAILED';
+        } else {
+            connectionStatus.textContent = 'CANCELLED';
+        }
     }
-  }
 }
 
 async function sendBLEData() {
-
-  var uint8array = new TextEncoder().encode(stringToSend.value);
-
-  armPropDataCharacteristic.writeValueWithoutResponse(uint8array);
-  console.log('Value has been written');
+    logger('Sending: ' + stringToSend.value);
+    try{
+        var uint8array = new TextEncoder().encode(stringToSend.value);
+        armPropDataCharacteristic.writeValueWithoutResponse(uint8array);
+        logger('Value has been written');
+    } catch(error){
+        logger('Error:' + error);
+    }
 }
 
 function getSupportedProperties(characteristic) {
-  let supportedProperties = [];
-  for (const p in characteristic.properties) {
-    if (characteristic.properties[p] === true) {
-      supportedProperties.push(p.toUpperCase());
+    let supportedProperties = [];
+    for (const p in characteristic.properties) {
+        if (characteristic.properties[p] === true) {
+            supportedProperties.push(p.toUpperCase());
+        }
     }
-  }
-  return '[' + supportedProperties.join(', ') + ']';
+    return '[' + supportedProperties.join(', ') + ']';
 }
 
-function handleNotifications(event) {
-    console.log("Notification received");
+function handleNotifications_arm_prop_data(event) {
     let value = event.target.value;
     let a = [];
-    
+
     for (let i = 0; i < value.byteLength; i++) {
         a.push(String.fromCharCode(value.getUint8(i)));
-        
     }
-    //joing using "" inplace of ","
-    console.log(a.join(''));
+    // joing using "" inplace of ","
+    logger('ARM Prop Data Notification: ' + a.join(''));
+}
 
-  }
+function logger(text) {
+    logArea.textContent += text + '\n';
+    logArea.scrollTop = logArea.scrollHeight;
+}
